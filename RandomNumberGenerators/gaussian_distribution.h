c68ad6dda7046bc9dd8d57412ab428eafe80fd51
Luca Morselli <luca.morselli8@gmail.com>
2024-04-20 13:22:50 +0200

    added a struct for representing a 2D gaussian distribution (no correlation between x,y)

--- RandomNumberGenerators/gaussian_distribution.h
+++ RandomNumberGenerators/gaussian_distribution.h
@@ -4,6 +4,21 @@
 #include <math.h>
 #include <stdlib.h>
 
+
+/*
+* A struct that defines a 2D Gaussian Distribution
+*/
+typedef struct GaussianDistribution{
+
+    double mu_x;
+    double mu_y;
+    double sigma_x;
+    double sigma_y;
+
+
+} GaussianDistribution;
+
+
 /*
 * Generate a gaussian distributed double with a certain
 * mean and sigma using the Box-Muller Algorithm.
@@ -15,6 +30,32 @@ double generate_gaussian(double mean, double sigma)
     double U2 = ((double)rand())/RAND_MAX;
 
     return ( sigma * sqrt(-2*log(U1)) * cos(2.0*M_PI*U2) ) + mean;
+}
+
+/*
+* Generate a dataset having "number_of_distributions" gaussian clusters 
+*
+*/
+void generate_gaussian_clusters_dataset(Point* point_array, int counts, GaussianDistribution* gaussian_distribution_array, int number_of_distributions)
+{
+
+    for (int i = 0; i < counts; i++)
+    {
+        
+        int selected_distribution = i%number_of_distributions;
+        
+        // Draw a sample from the selected distribution
+        // new point class is initialized to -1 as point is unclassified
+        Point p = {0,0,-1};
+        p.x = generate_gaussian(gaussian_distribution_array[selected_distribution].mu_x,gaussian_distribution_array[selected_distribution].sigma_x);
+        p.y = generate_gaussian(gaussian_distribution_array[selected_distribution].mu_y,gaussian_distribution_array[selected_distribution].sigma_y);
+        
+        // Save point to array
+        point_array[i] = p; 
+    }
+
+    return ;
+    
 }
 
 
